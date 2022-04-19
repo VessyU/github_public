b87d4b6c9bde85bc5504cef2d583f06163a32350
VessyU <vessy.ustabashiev@gmail.com>
2022-04-19 21:44:49 +0100

    iam user
    code for creating aws iam user

---
+++ Terraform/01-Terraform-Basics/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+#plan - execute
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-vessyu-123456-01"
+    versioning {
+        enabled = true
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc_updated"
+}
+
+#STATE
+#DESIRED - KNOWN - ACTUAL
+
+output "my_iam_user_complete_details" {
+    value = aws_iam_user.my_iam_user
+}
